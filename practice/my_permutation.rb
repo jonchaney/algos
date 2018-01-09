17b1c89440e0de825505a161afe2e7d41e9fd00c
Jonathan Chaney <jonchaney@gmail.com>
2018-01-09 13:55:10 -0800

    brute force permutations--no bueno

--- practice/my_permutation.rb
+++ practice/my_permutation.rb
@@ -1,4 +1,18 @@
 # find the permutations of a given str
 def my_permutations(str)
-
+  arr = str.chars
+  result = []
+  arr.length.times do |idx1| 
+    sub_str = arr[0...idx1] + arr[idx1+1..-1]
+    arr.length.times do |idx2|
+      perm = sub_str[0...idx2] + [arr[idx1]] + sub_str[idx2..-1]
+      result.push(perm) if !result.include?(perm)
+      result.push(perm.reverse) if !result.include?(perm.reverse)
+    end 
+  end 
+  result
 end 
+
+
+
+p my_permutations("abc")
