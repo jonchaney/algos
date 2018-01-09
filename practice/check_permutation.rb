aa9ef5946339cf77d1ab0da5fc4ed249b8a6e282
Jonathan Chaney <jonchaney@gmail.com>
2018-01-09 12:50:02 -0800

    my permutations

--- practice/check_permutation.rb
+++ practice/check_permutation.rb
@@ -1,4 +1,4 @@
 # Given two strings write a method to deteremine if one is a permutation of the other
 def check_permutation(str1, str2)
-
+  
 end
---
+++ practice/my_permutation.rb
@@ -0,0 +1,4 @@
+# find the permutations of a given str
+def my_permutations(str)
+
+end 
