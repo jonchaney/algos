9b631457cfee9059ddfe1aa89485a2990cf54d60
Jonathan Chaney <jonchaney@gmail.com>
2018-01-12 13:56:11 -0800

    check permutation optimized approach -- O(n) with hash

--- practice/check_permutation.rb
+++ practice/check_permutation.rb
@@ -8,9 +8,24 @@ def check_permutation_first(str1, str2)
   str1 == str2 # compare for equality: O(n)
 end
 
-p check_permutation("bca", "bac")
+# p check_permutation_first("bca", "bac")
 
-# optimized approach, uses space, O(n)
+# optimized approach
+# uses a hash (extra space), but time complexity is O(n)
 def check_permutation(str1, str2)
-  
+  return false if str1.length != str2.length
+  # put str1 into a hash -- O(n)
+  hash = {}
+  str1.each_char do |el|
+    hash[el] = true
+  end 
+
+  # check if str2 has all the same charaters as str1 using the hash
+  # O(n)
+  str2.each_char do |el|
+    return false if !hash[el]
+  end 
+  true
 end
+
+p check_permutation("baa", "bac")
