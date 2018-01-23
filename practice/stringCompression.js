45427d962fd1fb8c227bdfcb3d4cf66c576e657d
Jonathan Chaney <jonchaney@gmail.com>
2018-01-22 16:46:36 -0800

    string compression

--- practice/stringCompression.js
+++ practice/stringCompression.js
@@ -7,28 +7,18 @@
 // and the number of times they appear as values
 // then iterate over hash table and create string.
 const stringCompression = (str) => {
-  let counter = {};
-  let result = "";
-  // O(n)
+  let compressedString = "";
+  let countConsecutive = 0;
   for (let i = 0; i < str.length; i++) {
-    // console.log(counter[str[i]])
-    if (isNaN(counter[str[i]])) {
-      counter[str[i]] = 1;
-    } else {
-      counter[str[i]] += 1;
+    countConsecutive++;
+
+    // if next character is different than current, append this char to result.
+    if (i + 1 >= str.length || str.charAt(i) != str.charAt(i+1)) {
+      compressedString += "" + str.charAt(i) + countConsecutive;
+      countConsecutive = 0;
     }
   }
-
-  // O(n)
-  let keys = Object.keys(counter);
-
-  // O(m)
-  for (let i = 0; i < keys.length; i++) {
-    result = result.concat(keys[i], counter[keys[i]]);
-  }
-
-  return result;
+  return compressedString.length < str.length ? compressedString : str;
 };
-
 
 console.log(stringCompression("aabbccc"));
