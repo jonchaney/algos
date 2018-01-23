6876df93e02bf15d64caaa71d7d85ac5eecccc8b
Jonathan Chaney <jonchaney@gmail.com>
2018-01-22 16:51:42 -0800

    string compression

--- practice/stringCompression.js
+++ practice/stringCompression.js
@@ -2,10 +2,7 @@
 // input: aabcccccaaa
 // output: a2b1c5a3
 
-// O(n)
-// create hash map to store the letters that appear as keys
-// and the number of times they appear as values
-// then iterate over hash table and create string.
+// O(p + k^2)
 const stringCompression = (str) => {
   let compressedString = "";
   let countConsecutive = 0;
@@ -22,3 +19,21 @@ const stringCompression = (str) => {
 };
 
 console.log(stringCompression("aabbccc"));
+
+const stringCompressionOptimized = (str) => {
+  let compressed = [];
+  let countConsecutive = 0;
+  for (let i = 0; i < str.length; i++) {
+    countConsecutive++;
+
+    // if next character is different than current, append this char to result.
+    if (i + 1 >= str.length || str.charAt(i) != str.charAt(i + 1)) {
+      compressed.append(str.charAt(i));
+      compressed.append(countConsecutive);
+      countConsecutive = 0;
+    }
+  }
+  return compressed.length < str.length ? compressed.join("") : str;
+};
+
+console.log(stringCompressionOptimized("aabbccc"));
