c9eda76e7258a17a215fda82d81261d743d9c247
Jonathan Chaney <jonchaney@gmail.com>
2018-04-03 10:43:34 -0700

    hashing algos

--- 01b hash_map:lru_cache/lib/p02_hashing.rb
+++ 01b hash_map:lru_cache/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    val = 0
+    self.each_with_index do |el, idx|
+      val += (el + idx).hash
+    end
+    val
   end
 end
 
 class String
   def hash
+    val = 0
+    self.chars.each_with_index do |el, idx|
+      val += (el.ord + idx).hash
+    end 
+    val
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    val = 0
+    self.keys.each_with_index do |el, idx|
+      val = (el.to_s.to_i + idx).hash
+    end
+    val 
   end
 end
