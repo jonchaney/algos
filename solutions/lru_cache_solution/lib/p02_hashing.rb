69c2690b95f0ae8610e3c404c13e28b6d26ed45d
Jonathan Chaney <jonchaney@gmail.com>
2018-04-03 11:00:28 -0700

    hashing clarifications + solution notes

--- solutions/lru_cache_solution/lib/p02_hashing.rb
+++ solutions/lru_cache_solution/lib/p02_hashing.rb
@@ -3,6 +3,8 @@ class Fixnum
 end
 
 class Array
+  # added inject(0) to each_with_index give the another argument that starts at zero between the pipes
+  # ever iteration that value changes to the value of the equation in the block -- (el.hash + i.hash) + intermediate_hash
   def hash
     each_with_index.inject(0) do |intermediate_hash, (el, i)|
       (el.hash + i.hash) ^ intermediate_hash
