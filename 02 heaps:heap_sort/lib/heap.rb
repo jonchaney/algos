6104bdd2e359d9ab5878a4270fd5c8993ca0f5ff
Jon Chaney <jonchaney@gmail.com>
2018-04-18 15:16:48 -0700

    heap implemention done

--- 02 heaps:heap_sort/lib/heap.rb
+++ 02 heaps:heap_sort/lib/heap.rb
@@ -11,6 +11,11 @@ class BinaryMinHeap
   end
 
   def extract
+    @store[0], @store[@count - 1] = @store[@count - 1], @store[0]
+    val = @store.pop
+    @count -= 1
+    BinaryMinHeap.heapify_down(@store, 0, @store.length, &prc)
+    val
   end
 
   def peek
@@ -18,6 +23,9 @@ class BinaryMinHeap
   end
 
   def push(val)
+    @store.push(val)
+    @count += 1
+    BinaryMinHeap.heapify_up(@store, @count - 1, @store.length, &prc)
   end
 
   def self.child_indices(len, parent)
@@ -39,15 +47,20 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(array, parent, len = array.length, &prc)
+    unless prc 
+      prc = Proc.new do |el1, el2|
+        el1 > el2
+      end
+    end
     children = BinaryMinHeap.child_indices(len, parent)
     return nil if children.nil?
     left_child = children[0]
     right_child = children[1]
-    if array[parent] > array[left_child]
+    if prc.call(array[parent], array[left_child])
       array[parent], array[left_child] = array[left_child], array[parent]
       BinaryMinHeap.heapify_down(array, parent + 1, len, &prc)
     elsif right_child
-      if array[parent] > array[right_child]
+      if prc.call(array[parent], array[right_child])
         array[parent], array[right_child] = array[right_child], array[parent]
         BinaryMinHeap.heapify_down(array, parent + 1, len, &prc)
       end
@@ -56,9 +69,17 @@ class BinaryMinHeap
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
-    return array if child_idx.zero?
+    unless prc
+      prc = Proc.new do |el1, el2|
+        el1 > el2
+      end
+    end
+    return nil if child_idx.zero?
     parent = BinaryMinHeap.parent_index(child_idx)
-    array[parent], array[child_idx] = array[child_idx], array[parent]
-    BinaryMinHeap.heapify_up(array, parent, len, &prc)
+    if prc.call(array[parent], array[child_idx])
+      array[parent], array[child_idx] = array[child_idx], array[parent]
+      BinaryMinHeap.heapify_up(array, parent, len, &prc)
+    end 
+    array
   end
 end
