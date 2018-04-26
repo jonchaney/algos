e2ed7e50ae3d96bc95a8baf139b6bce039f84d99
Jon Chaney <jonchaney@gmail.com>
2018-04-25 17:02:01 -0700

    more efficient intersection linked list

--- practice/intersection.rb
+++ practice/intersection.rb
@@ -6,12 +6,12 @@ head_one = createLinkedList(["d","a","d","a","d"])
 head_two = Node.new
 head_two.value = "b"
 head_two.next = Node.new("a")
-head_two.next.next = Node.new('b')
-head_two.next.next.next = head_one.next.next
+# head_two.next.next = Node.new('b')
+head_two.next.next = head_one.next.next
 # puts 'List One'
-# printLinkedList(head_one)
-# puts 'List Two'
-# printLinkedList(head_two)
+printLinkedList(head_one)
+puts 'List Two'
+printLinkedList(head_two)
 
 def intersection(head_one, head_two)
     # get lengths of lists -- O(n)
@@ -61,4 +61,52 @@ def getLength(node)
     return length
 end 
 
-p intersection(head_one, head_two)
+def intersectionBetter(head_one, head_two)
+    return false if head_one.nil? || head_two.nil?
+
+    # get tails and lengths
+    tail_one = head_one
+    len_one = 0
+    while tail_one.next
+        tail_one = tail_one.next 
+        len_one += 1
+    end 
+    
+    tail_two = head_two
+    len_two = 0
+    while tail_two.next
+        tail_two = tail_two.next 
+        len_two += 1
+    end 
+
+    # if same length iterate over lists until at same pointer
+    if (len_one == len_two) 
+        while head_one != head_two
+            head_one = head_one.next
+            head_two = head_two.next
+        end 
+        return head_one
+    end 
+
+    return false if tail_one != tail_two
+
+    shorter = len_one < len_two ? head_one : head_two
+    longer = len_one > len_two ? head_one : head_two
+
+    # advance the longer pointer by difference in lengths
+    i = 0
+    while i < (len_one - len_two).abs
+        longer = longer.next
+        i+=1
+    end 
+
+    while shorter != longer
+        shorter = shorter.next
+        longer = longer.next
+    end 
+
+
+    return longer
+end 
+
+p intersectionBetter(head_one, head_two)
